6491268ee777ab8bf4b6cf06029b7abc73f36e47
Rychillie <rychillie@rychillie.net>
2024-07-19 19:01:22 -0300

    Permissions with conditionals

--- packages/auth/src/models/index.ts
+++ packages/auth/src/models/index.ts
@@ -1,3 +1,12 @@
-import type { User } from './user'
+import { type Organization, organizationSchema } from './organization'
+import { type Project, projectSchema } from './project'
+import { type User, userSchema } from './user'
 
-export { User }
+export {
+  Organization,
+  organizationSchema,
+  Project,
+  projectSchema,
+  User,
+  userSchema,
+}
