8cb8d7fa59517b4b81d95bdfbbd2099a5a045812
Rychillie <rychillie@rychillie.net>
2024-07-25 19:21:15 -0300

    Route: Pending invitations

--- apps/api/src/http/routes/invites/index.ts
+++ apps/api/src/http/routes/invites/index.ts
@@ -2,6 +2,7 @@ import { acceptInvite } from './accept-invite'
 import { createInvite } from './create-invite'
 import { getInvite } from './get-invite'
 import { getInvites } from './get-invites'
+import { getPendingInvites } from './get-pending-invites'
 import { rejectInvite } from './reject-invite'
 import { revokeInvite } from './revoke-invite'
 
@@ -10,6 +11,7 @@ export {
   createInvite,
   getInvite,
   getInvites,
+  getPendingInvites,
   rejectInvite,
   revokeInvite,
 }
