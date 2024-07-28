1bdbe3b00ae15562b418adbfde6bc311b5060397
Rychillie <rychillie@rychillie.net>
2024-07-28 00:02:49 -0300

    Using the useActionState hook

--- apps/web/src/app/auth/sign-in/actions.tsx
+++ apps/web/src/app/auth/sign-in/actions.tsx
@@ -2,8 +2,15 @@
 
 import { signInWithPassword } from '@/http/sign-in-with-password'
 
-export async function signInWithEmailAndPassword(data: FormData) {
+export async function signInWithEmailAndPassword(
+  previeousState: unknown,
+  data: FormData,
+) {
+  console.log('previeousState', previeousState)
+
   const { email, password } = Object.fromEntries(data)
+
+  await new Promise((resolve) => setTimeout(resolve, 2000))
 
   const result = signInWithPassword({
     email: String(email),
@@ -11,4 +18,6 @@ export async function signInWithEmailAndPassword(data: FormData) {
   })
 
   console.log(result)
+
+  return 'Success'
 }
